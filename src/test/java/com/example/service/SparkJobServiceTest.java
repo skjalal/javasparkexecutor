7493885702f85b0b26d44c367d6f5e529e8cbdca
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2020-06-18 14:23:44 +0530

    Added Test Class For SparkJobService Class

---
+++ src/test/java/com/example/service/SparkJobServiceTest.java
@@ -0,0 +1,40 @@
+package com.example.service;
+
+import com.example.config.SparkConfiguration;
+import com.example.config.SparkProperties;
+import kafka.EmbeddedSingleNodeKafkaCluster;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkJobServiceTest {
+
+    @RegisterExtension
+    public EmbeddedSingleNodeKafkaCluster cluster = new EmbeddedSingleNodeKafkaCluster();
+
+    private static final String TOPIC_NAME = "TestTopic";
+
+    private SparkJobService sparkJobService;
+
+    @Autowired
+    SparkProperties sparkProperties;
+
+    @Autowired
+    SparkConfiguration sparkConfiguration;
+
+    @BeforeEach
+    void setUp() {
+        cluster.produceSampleRecords(TOPIC_NAME);
+        sparkJobService = Mockito.spy(new SparkJobService(sparkProperties, sparkConfiguration));
+    }
+
+    @Test
+    void testExecute() {
+        sparkJobService.execute();
+        Mockito.verify(sparkJobService).execute();
+    }
+}
