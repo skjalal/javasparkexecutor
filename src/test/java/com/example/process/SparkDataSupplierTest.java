c864297af39f67d01559597e9e7fccf0742866a9
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2020-06-18 15:43:21 +0530

    Added Test Class For SparkDataSupplier Class

---
+++ src/test/java/com/example/process/SparkDataSupplierTest.java
@@ -0,0 +1,50 @@
+package com.example.process;
+
+import com.example.config.SparkConfiguration;
+import com.example.config.SparkProperties;
+import kafka.EmbeddedSingleNodeKafkaCluster;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkDataSupplierTest {
+
+    @RegisterExtension
+    public EmbeddedSingleNodeKafkaCluster cluster = new EmbeddedSingleNodeKafkaCluster();
+
+    private static final String TOPIC_NAME = "TestTopic";
+    private SparkDataSupplier sparkDataSupplier;
+    private SparkSession sparkSession;
+
+    @Autowired
+    SparkProperties sparkProperties;
+
+    @Autowired
+    SparkConfiguration sparkConfiguration;
+
+    @BeforeEach
+    void setUp() {
+        cluster.produceSampleRecords(TOPIC_NAME);
+        sparkSession = sparkConfiguration.getSparkSession();
+        sparkDataSupplier = new SparkDataSupplier(sparkSession, sparkProperties);
+    }
+
+    @Test
+    void testGet() {
+        Dataset<Row> rowDataset = sparkDataSupplier.get();
+        Assertions.assertNotNull(rowDataset);
+    }
+
+    @AfterEach
+    void tearDown() {
+        sparkSession.close();
+    }
+}
