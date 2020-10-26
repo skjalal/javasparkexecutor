395e018fb4b7d98d227fad80c2adac8f7934314d
Shaikh Jalaluddin <sk.jalal460@gmail.com>
2020-10-26 14:34:44 +0530

    Fixed Embedded IP for Kafka Server

--- src/test/java/com/example/JavaSparkExecutorTest.java
+++ src/test/java/com/example/JavaSparkExecutorTest.java
@@ -1,28 +1,20 @@
 package com.example;
 
-import com.example.config.SparkConfiguration;
 import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
 import kafka.EmbeddedSingleNodeKafkaCluster;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
 
-@SpringBootTest
 class JavaSparkExecutorTest {
 
     @RegisterExtension
     public EmbeddedSingleNodeKafkaCluster cluster = new EmbeddedSingleNodeKafkaCluster();
 
     private static final String TOPIC_NAME = "TestTopic";
-
-    @Autowired
-    SparkConfiguration sparkConfiguration;
 
     @BeforeEach
     void setUp() {
@@ -34,10 +26,5 @@ class JavaSparkExecutorTest {
     void testMain() throws IOException, RestClientException {
         JavaSparkExecutor.main(new String[]{});
         Assertions.assertTrue(true);
-    }
-
-    @AfterEach
-    void tearDown() {
-        sparkConfiguration.closeSparkSession();
     }
 }
